c9c65fa342e677b0e43285c8dad6391eae1f1c19
federicogiustozzi <federicogiustozzi00@gmail.com>
2024-05-21 12:29:56 +0200

    commit 3

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 //istantiate the classes
 const express = require("express");
+const mysql = require("mysql");
 const path = require("path");
 var bodyParser = require('body-parser')
 
 const app = express();
+
+// access to database
+const db = mysql.createConnection({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD, 
+  database: process.env.DATABASE
+})
+
 
 
 // mount and use public directory
@@ -25,6 +35,17 @@ app.use("/", require("./routes/pages.js"))
 // manage the post requests at the /json address 
 app.post("/json", function (req, res) {
     console.log(req.body) // populated!
+
+    // and now connect to database
+    db.connect ( (error) => {
+      if(error) {
+          console.log(error)
+      }
+      else
+      {
+          console.log("MYSQL Connected...")
+      }
+  })
   });
 
 
