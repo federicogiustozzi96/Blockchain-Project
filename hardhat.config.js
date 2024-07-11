43e45b829ff6b9b37c36bfffe7fa6236a22a8475
Andrea Nisio <andreanisio97@gmail.com>
2024-07-11 12:40:37 +0200

    - creazione script per deploy del contrato Token - aggiunta della rete donutsNetwork (Definita su Metamask) su hardhat.config.js
    Note: Quando si deve deployare un contratto, lo si deve fare su donutsNetwork e non su Hardhat network.
    
    il comando per deployare sarà quindi:
    
    npx hardhat run scripts/deployToken.js --network donutsNetwork

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,12 +3,25 @@ require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
   networks: {
     hardhat: {
       accounts: {
         mnemonic: process.env.SEED_PHRASE,
       },
       chainId: 1337
+    },
+    donutsNetwork: {
+      url: "http://127.0.0.1:8545",
+      chainId: 1337,
+      accounts: {
+        mnemonic: process.env.SEED_PHRASE,
+      },
     },
   },
 };
---
+++ scripts/deployToken.js
@@ -0,0 +1,38 @@
+// scripts/deploy.js
+
+/**
+ * dichiarata come asincrona (async) perché contiene operazioni asincrone come il deploy del contratto.
+ */
+async function main() {
+
+    /**
+     * Utilizza ethers.getSigners() per ottenere un array di signers (firmatari). 
+     * In questo caso, estrae il primo elemento (il deployer) e lo assegna alla variabile deployer.
+     */
+    const [deployer] = await ethers.getSigners();
+
+    //Stampa sull'output la stringa "Deploying contracts with the account:" seguita dall'indirizzo del deployer.
+    console.log("Deploying contracts with the account:", deployer.address);
+
+    /**
+     * Utilizza ethers.getContractFactory() per ottenere un'istanza della factory (struttura utilizzata per creare istanze di un contratto su Ethereum) del 
+     * contratto Token dal compilatore di Solidity.
+     */
+    const Token = await ethers.getContractFactory("Token");
+
+    /**
+     *  Esegue il deploy del contratto utilizzando il metodo .deploy() della factory ottenuta precedentemente. 
+     * Questo comando effettivamente carica il contratto sulla blockchain.
+     */
+    const token = await Token.deploy();
+
+    //Stampa l'indirizzo del contratto appena deployato sulla blockchain.
+    console.log("Token deployed to:", token.address);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch(error => {
+        console.error(error);
+        process.exit(1);
+    });
